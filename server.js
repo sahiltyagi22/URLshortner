751195632430ca3f928b6dea3d035642b83fadc7
sahil <sahiltyagi1204@outlook.com>
2023-07-05 19:14:38 +0530

    added url and short url routes

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 const express = require('express')
 const ejs = require('ejs')
-
+const mongoose = require('mongoose')
+const db = require('./models/db')
 
 const app = express()
 
-app.set('view engine' , 'ejs')
 
-app.get('/' , (req,res)=>{
-    res.render('index')
+mongoose.connect('mongodb://127.0.0.1/URL' , {
+    useNewUrlParser : true, 
+    useUnifiedTopology : true
 })
 
+
+
+app.use(express.urlencoded({extended: true}))
+app.set('view engine' , 'ejs')
+
+app.get('/' , async (req,res)=>{
+    const urls = await db.find()
+    console.log(urls);
+    res.render('index', {urls : urls}) 
+})
+
+app.post('/shortUrl' , async(req,res)=>{
+     await db.create({full : req.body.url})
+
+     res.redirect('/')
+})
 
 
 
